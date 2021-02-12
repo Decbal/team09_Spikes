8b3c1ea464840f9c406d2856eb4ee9ad741cc9c9
Decbal <williambrante@live.se>
2021-02-12 21:35:42 +0100

    Added class that contains global strings.

---
+++ server/GlobalStrings.java
@@ -0,0 +1,12 @@
+package server;
+
+public class GlobalStrings {
+    public static final String OP_TEST =        "TEST";
+    public static final String OP_GET =         "GET";
+    public static final String OP_SAVE =        "SAVE";
+    public static final String OP_SAVEFILE =    "SAVEFILE";
+    public static final String OP_READ =        "READ";
+    public static final String TYPE_START =     "START";
+    public static final String TYPE_END =       "END";
+    public static final String TYPE_NAME =      "NAME";
+}
